86a0970bf3f4a37eba11a60c2d5e430a69fcfb4e
Harsh Kumar <harshkumarvc@gmail.com>
2019-11-30 22:40:56 +0530

    bahot kuch

--- src/sample/selectLevelController.java
+++ src/sample/selectLevelController.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -8,14 +9,16 @@ public class selectLevelController implements EventHandler<MouseEvent> {
 
     int level;
     Stage primaryStage;
+    Scene mainMenuScene;
 
-    public selectLevelController(Stage ps,int l){
+    public selectLevelController(Stage ps,int l,Scene mms){
         this.primaryStage = ps;
         this.level = l;
+        this.mainMenuScene = mms;
     }
     @Override
     public void handle(MouseEvent mouseEvent) {
-        gameLevel gamelevel = new gameLevel(level);
+        gameLevel gamelevel = new gameLevel(level,mainMenuScene);
         try {
             gamelevel.start(primaryStage);
         } catch (Exception e) {
